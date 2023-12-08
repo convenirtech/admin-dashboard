38febca2c0aa36603fbfb548aad85d44df918ba3
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-12-08 22:01:05 +0200

    local

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className=' px-5 max-w-[1280px] mx-auto'>
-    <NavBar/>
+    <div className=' mx-auto'>
+    {/* <NavBar/> */}
+    <main className=' bg-gray-100 min-h-screen'>
+    <Header/>
+    </main>
   </div>
   )
 }
